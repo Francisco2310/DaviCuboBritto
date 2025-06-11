a6f0cc685617b74c8e14bb0891d28c96cf2a53b3
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2025-06-11 17:22:28 -0300

    camera controls added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -13,16 +14,30 @@ const texture = new THREE.VideoTexture(video);
 const material = new THREE.MeshBasicMaterial( { map:texture } );
 const cube = new THREE.Mesh( geometry, material );
 const renderer = new THREE.WebGLRenderer();
+let angle = 0;
+const radius = 2;
 
 scene.add( cube );
 camera.position.z = 1;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; 
+controls.dampingFactor = 0.05;
+controls.target.set(0, 0, 0);
+
+
+
 function animate() {
     requestAnimationFrame( animate );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    angle += 0.01;
+    cube.position.x = radius * Math.cos(angle);
+    cube.position.z = radius * Math.sin(angle);
+    controls.update();
     renderer.render( scene, camera );
 }
 
